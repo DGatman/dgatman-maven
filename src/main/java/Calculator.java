c86f749ca528adb5972780c9f05fa7532d06d1e0
DolphieDude <andreycherevat@gmail.com>
2021-10-07 21:16:45 +0300

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    static public int sum(int a,int b){
+        return a+b;
+    }
+    static public int mult(int a,int b){
+        return a*b;
+    }
+    static public double div(int a,int b){
+        return a/b;
+    }
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void test_sum_normal_case(){
+        Assertions.assertEquals(4,Calculator.sum(3,1));
+    }
+    @Test
+    public void test_division_zero(){
+        Assertions.assertThrows(ArithmeticException.class,() -> Calculator.div(5,0));
+    }
+}
